91dd58b2c03809ea4911d504db2b862184920d85
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-10 16:02:29 +0600

    new atribute

--- src/main/java/kz/baltabayev/security/Authentication.java
+++ src/main/java/kz/baltabayev/security/Authentication.java
@@ -1,5 +1,6 @@
 package kz.baltabayev.security;
 
+import kz.baltabayev.model.types.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Authentication {
     private String login;
+    private Role role;
     private boolean isAuth;
     private String message;
 }
